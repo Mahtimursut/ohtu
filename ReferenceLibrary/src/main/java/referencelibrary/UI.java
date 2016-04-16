f820b252178d0426a7a5303ceede0b1acc234941
Pietrorossellini <pietrorossellini@gmail.com>
2016-04-16 19:05:20 +0300

    Add UI support for all reference fields

--- ReferenceLibrary/src/main/java/referencelibrary/UI.java
+++ ReferenceLibrary/src/main/java/referencelibrary/UI.java
@@ -84,24 +84,71 @@ public class UI {
     public void addNewBook() {
         //prompt reference name
         String referenceName = io.readLine("Reference id");
+
         Reference newRef = new BookReference(referenceName);
-        
-        //prompt fields
-        String author = io.readLine("Author: ");
-        String title = io.readLine("Title: ");
-        String year = io.readLine("Year: ");
-        String publisher = io.readLine("Publisher: ");
-        
-        //set fields
-        newRef.setField("author", author);
-        newRef.setField("title", title);
-        newRef.setField("year", year);
-        newRef.setField("publisher", publisher);
+        fillReferenceFields(newRef);
 
         //save the reference
         app.newReference(newRef);
     }
-    
+
+    private void fillReferenceFields(Reference reference) {
+        fillRequiredFields(reference);
+
+        // TODO maybe a yes/no UI function should be generalized
+        String optionalAnswer = io.readLine("Would you like to add some optional fields?\n" +
+                "\t(y)es\n" +
+                "\t(n)o (default)\n");
+        if (optionalAnswer.equalsIgnoreCase("y")) {
+            fillOptionalFields(reference);
+        }
+    }
+
+    private void fillRequiredFields(Reference reference) {
+        reference.getRequiredFields().forEach(field -> reference.setField(field, promptForField(field)));
+    }
+
+    private void fillOptionalFields(Reference reference) {
+        while (true) {
+            String command = io.readLine("How do you want to proceed?\n" +
+                    "\t(s)how valid field names for this reference type\n" +
+                    "\t(a)dd a field\n" +
+                    "\t(d)one with adding fields\n");
+
+            switch (command) {
+                case "s":
+                    showValidOptionalFields(reference);
+                    break;
+                case "a":
+                    addOptionalFieldToReference(reference);
+                    break;
+                case "d":
+                default:
+                    return;
+            }
+        }
+    }
+
+    private void addOptionalFieldToReference(Reference reference) {
+        String fieldName;
+
+        // TODO should have some "backend" validation also (e.g. exception from Reference)
+        do {
+            fieldName = io.readLine("Field:");
+        } while (!reference.getOptionalFields().contains(fieldName));
+
+        String value = io.readLine("Value for " + fieldName);
+        reference.setField(fieldName, value);
+    }
+
+    private String promptForField(String fieldName) {
+        return io.readLine(fieldName + ":");
+    }
+
+    private void showValidOptionalFields(Reference reference) {
+        reference.getOptionalFields().forEach(io::print);
+    }
+
     public void generateBixTexFile() {
         io.print("\nGenerating bibTex -file");
         //call bibtex-file generator
