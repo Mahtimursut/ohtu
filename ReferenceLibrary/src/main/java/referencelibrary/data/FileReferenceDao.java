cb107e75a84db2a3a608365692e0195cd6933ff3
Rochet2 <tqbattlenet@gmail.com>
2016-04-18 14:55:54 +0300

    Kill resource close mutations

--- ReferenceLibrary/src/main/java/referencelibrary/data/FileReferenceDao.java
+++ ReferenceLibrary/src/main/java/referencelibrary/data/FileReferenceDao.java
@@ -10,11 +10,13 @@ import java.util.List;
  * Creted by petri on 11/04/16
  */
 public class FileReferenceDao implements ReferenceDao {
-    private File file;
+
+    private final File file;
     private List<Reference> references;
 
     /**
      * Creates a FileReferenceDao that uses a file with a given name for storage
+     *
      * @param filename filename for storing References
      */
     public FileReferenceDao(String filename) {
@@ -24,6 +26,7 @@ public class FileReferenceDao implements ReferenceDao {
 
     /**
      * Reads stored References
+     *
      * @return List of References
      */
     private List<Reference> readReferences() {
@@ -36,16 +39,17 @@ public class FileReferenceDao implements ReferenceDao {
 
     /**
      * Reads the Reference List from the file
+     *
      * @return List of References
      * @throws IOException
      * @throws ClassNotFoundException
      */
     private List<Reference> readFromStream() throws IOException, ClassNotFoundException {
-        FileInputStream fis = new FileInputStream(file);
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        List<Reference> refs = (List<Reference>)ois.readObject();
-        ois.close();
-        fis.close();
+        List<Reference> refs;
+        try (FileInputStream fis = new FileInputStream(file);
+                ObjectInputStream ois = new ObjectInputStream(fis)) {
+            refs = (List<Reference>) ois.readObject();
+        }
         return refs;
     }
 
@@ -67,15 +71,15 @@ public class FileReferenceDao implements ReferenceDao {
 
     /**
      * Writes the Reference List to the file
+     *
      * @throws IOException
      */
     private void writeReferenceListToFile() throws IOException {
-        FileOutputStream fos = new FileOutputStream(file);
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        try (FileOutputStream fos = new FileOutputStream(file);
+                ObjectOutputStream oos = new ObjectOutputStream(fos)) {
 
-        oos.writeObject(references);
-        oos.flush();
-        oos.close();
-        fos.close();
+            oos.writeObject(references);
+            oos.flush();
+        }
     }
 }
