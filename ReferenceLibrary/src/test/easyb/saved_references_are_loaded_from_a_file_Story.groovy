427d69e83c97c9f957c1eb19aecb6540d3739028
TaikaMurmeli <sami.sarsa@helsinki.fi>
2016-04-27 11:09:05 +0300

    easyB testaus tiedoston lukemiseen

---
+++ ReferenceLibrary/src/test/easyb/saved_references_are_loaded_from_a_file_Story.groovy
@@ -0,0 +1,53 @@
+/**
+ * Created by sami on 27.4.2016.
+ */
+
+import referencelibrary.io.StubIO
+import referencelibrary.App
+import referencelibrary.data.*
+import referencelibrary.ui.UI
+
+description 'References get loaded from a saved file'
+
+scenario "New reference is created and saved in a file", {
+    given 'Reference is created and saved in a file', {
+        //create a reference and quit
+        io = new StubIO("a", 
+            "b", 
+            "a", 
+            "name",
+            "author", 
+            "publisher",
+            "title", 
+            "year", 
+            "n", 
+            "s",
+            "q")
+        app = new App(new FileReferenceDao("saved_references"))
+        ui = new UI(io, app)
+    }
+    when 'reference is created and show command selected', {
+        ui.run()
+    }
+    then 'the added reference is found', {
+        io.getPrints().shouldHave("[name] author: title")
+    }
+}
+
+scenario "Created reference is loaded from a file", {
+    given 'The app is launched again', {
+        io = new StubIO(
+            "s",
+            "q")
+        app = new App(new FileReferenceDao("saved_references"))
+        ui = new UI(io, app)
+    }
+    when 'show command is selected', {
+        ui.run()
+    }
+    then 'the previously added reference is found', {
+        io.getPrints().shouldHave("[name] author: title")
+        // remove created reference
+        app.removeReference("name")
+    }
+}
